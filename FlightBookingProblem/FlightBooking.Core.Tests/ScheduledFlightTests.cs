a47982d51a216ea0abc0bdeb1a6454cb330ddfd7
Ashish Tamhane <ashish.tamhane@elekta.com>
2022-11-05 18:31:00 +0000

    Added unit test

--- FlightBookingProblem/FlightBooking.Core.Tests/ScheduledFlightTests.cs
+++ FlightBookingProblem/FlightBooking.Core.Tests/ScheduledFlightTests.cs
@@ -40,6 +40,34 @@ namespace FlightBooking.Core.Tests
         }
 
         [TestMethod]
+        public void TestGetFlightInformation()
+        {
+
+            FlightInformation flightInformation = scheduledFlight.GetFlightInformation();
+            FlightFinance flightFinance = new FlightFinance(scheduledFlight, scheduledFlight.FlightRoute, new ProfitCalculator());
+
+            flightInformation.costOfFlight = flightFinance.CostOfFlight();
+            flightInformation.profitFromFlight = flightFinance.ProfitFromFlight();
+            flightInformation.profitSurplus = flightFinance.ProfitSurplus();
+            flightInformation.totalLoyaltyPointsAccrued = flightManager.TotalLoyaltyPointsAccrued;
+            flightInformation.totalLoyaltyPointsRedeemed = flightManager.TotalLoyaltyPointsRedeemed;
+
+            Assert.AreEqual(12, flightInformation.aircraftNumberOfSeats);
+            Assert.AreEqual(500, flightInformation.costOfFlight);
+            Assert.AreEqual(13, flightInformation.expectedBaggageFromFlight);
+            Assert.AreEqual(0.7, flightInformation.flightRouteMinimumTakeOffPercentage);
+            Assert.AreEqual("London to Paris", flightInformation.flightRouteTitle);
+            Assert.AreEqual(800, flightInformation.profitFromFlight);
+            Assert.AreEqual(300, flightInformation.profitSurplus);
+            Assert.AreEqual(10, flightInformation.seatsTaken);
+            Assert.AreEqual(10, flightInformation.totalLoyaltyPointsAccrued);
+            Assert.AreEqual(100, flightInformation.totalLoyaltyPointsRedeemed);
+            
+
+            Assert.AreEqual(10, flightInformation.seatsTaken);
+        }
+
+        [TestMethod]
         public void TestBaggageFromFlight()
         {
             int output = scheduledFlight.GetExpectedBaggageFromFlight();
