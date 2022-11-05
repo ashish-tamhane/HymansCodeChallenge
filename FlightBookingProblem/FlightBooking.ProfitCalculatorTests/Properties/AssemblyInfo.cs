de6f49746bc3d607709d9ed860ae986c596473af
Ashish Tamhane <ashish.tamhane@elekta.com>
2022-11-05 18:21:23 +0000

    Added ProfitCalculatorTests

---
+++ FlightBookingProblem/FlightBooking.ProfitCalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlightBooking.ProfitCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ELEKTA")]
+[assembly: AssemblyProduct("FlightBooking.ProfitCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright © ELEKTA 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a179f295-4a6b-4d72-82c9-d990d1b2ab66")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
