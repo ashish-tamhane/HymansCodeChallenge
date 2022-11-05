7b4549ba07615dca2f1b37dbd22ca8ea24cb8554
Ashish Tamhane <ashish.tamhane@elekta.com>
2022-11-05 18:18:52 +0000

    Added LoyaltyCalculatorTests

---
+++ FlightBookingProblem/FlightBooking.LoyaltyPointsCalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlightBooking.LoyaltyPointsCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ELEKTA")]
+[assembly: AssemblyProduct("FlightBooking.LoyaltyPointsCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright © ELEKTA 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61e1b916-ef1d-40e0-b41b-65bff9e82808")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
